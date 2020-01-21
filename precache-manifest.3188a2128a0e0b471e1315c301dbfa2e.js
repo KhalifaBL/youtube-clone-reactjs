adbd87d1113bfee9645fe504aaffdd77fbc6a506
Khalifa Ben Lazrek <khalifalazreg@gmail.com>
2020-01-21 21:25:17 +0100

    Updates

---
+++ precache-manifest.3188a2128a0e0b471e1315c301dbfa2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a002312d078939904cac7e359c32840f",
+    "url": "/youtube-clone-reactjs/index.html"
+  },
+  {
+    "revision": "0b51e85ff8113bb7f64e",
+    "url": "/youtube-clone-reactjs/static/css/main.9f183c77.chunk.css"
+  },
+  {
+    "revision": "8f480e5e1eae402f6749",
+    "url": "/youtube-clone-reactjs/static/js/2.84c2a2c8.chunk.js"
+  },
+  {
+    "revision": "0b51e85ff8113bb7f64e",
+    "url": "/youtube-clone-reactjs/static/js/main.ded49e7d.chunk.js"
+  },
+  {
+    "revision": "d45ea6cd72d5b243558e",
+    "url": "/youtube-clone-reactjs/static/js/runtime-main.1a7864ad.js"
+  }
+]);

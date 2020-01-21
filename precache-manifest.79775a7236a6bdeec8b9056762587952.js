5bc66a6b7ce25edded8950e74439fab94b13f4de
Khalifa Ben Lazrek <khalifalazreg@gmail.com>
2020-01-21 21:49:43 +0100

    Updates

--- precache-manifest.79775a7236a6bdeec8b9056762587952.js
+++ precache-manifest.79775a7236a6bdeec8b9056762587952.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f312dd80033c4c3b40834bf69c9c6846",
+    "revision": "c32a7f109f46632d7874d6537c6a45d8",
     "url": "/youtube-clone-reactjs/index.html"
   },
   {
-    "revision": "03701f95c9e9c6f88a45",
-    "url": "/youtube-clone-reactjs/static/css/main.35edae99.chunk.css"
+    "revision": "5277af12ee5dc88d72b7",
+    "url": "/youtube-clone-reactjs/static/css/main.af67dd04.chunk.css"
   },
   {
     "revision": "8f480e5e1eae402f6749",
     "url": "/youtube-clone-reactjs/static/js/2.84c2a2c8.chunk.js"
   },
   {
-    "revision": "03701f95c9e9c6f88a45",
-    "url": "/youtube-clone-reactjs/static/js/main.38155671.chunk.js"
+    "revision": "5277af12ee5dc88d72b7",
+    "url": "/youtube-clone-reactjs/static/js/main.f61429ba.chunk.js"
   },
   {
     "revision": "d45ea6cd72d5b243558e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/youtube-clone-reactjs/precache-manifest.96ace1349ce342a4eb443f6fd154cfaa.js"
+  "/youtube-clone-reactjs/precache-manifest.79775a7236a6bdeec8b9056762587952.js"
 );
 
 self.addEventListener('message', (event) => {
